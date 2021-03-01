553e62099d67e2f389abe92119d7d9114ddde59a
Simon Gowing <simongowing1@gmail.com>
2021-03-01 18:01:30 +0100

    done

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -5,11 +5,26 @@ import { Link } from 'react-router-dom';
 class Beers extends React.Component {
 
     state = {
-        beers: this.props.location.state.beers
+        beers: this.props.location.state.beers,
+        query: ''
     }
 
+    handleChange = event => {
+        console.log(event.target.value)
+        let searchQuery = event.target.value.toLowerCase;
+      
+        this.setState(() => ({
+          query: searchQuery,
+        }))
+      }
+
   render(){
-let renderBeers = this.props.location.state.beers.map(beer => {
+
+    const searchResults = this.props.location.state.beers.filter((beer) => {
+        return beer.name.toLowerCase().includes(this.state.query)  
+          })
+
+let renderBeers = searchResults.map(beer => {
     return (
         <div key={beer._id}>
     <DetailedBeers name={beer.name} img={beer.image_url} tagline={beer.tagline} contributedBy={beer.contributed_by} id={beer._id} key={beer._id}/>
@@ -26,6 +41,16 @@ let renderBeers = this.props.location.state.beers.map(beer => {
     return (
         <div >
             <h1>All beers</h1>
+            <form >
+                <label htmlFor="query">Search: </label>
+                <input
+                type="text"
+                id="query"
+                name="query"
+                value={this.state.query}
+                onChange={this.handleChange}
+                />
+            </form>
             {renderBeers}
         </div>
       );
